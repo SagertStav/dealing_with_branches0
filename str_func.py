ad297cebe8f2b4fd01928dbe26faf6592217f0f9
SagertStav <biryukovsrg@yandex.ru>
2023-06-10 18:08:46 +0300

    Добавил вторую функцию hi_first_letters_of_words(any_text), докстринги, убрал мусор

--- str_func.py
+++ str_func.py
@@ -1,4 +1,13 @@
 def hi_reg_str(any_string):
+# Все буквы прописными возвращаются
+
     return any_string.upper()
 
-#print(hi_reg_str("drova"))
+
+def hi_first_letters_of_words(any_text):
+"""Функция hi_first_letters_of_words(any_text) возвращает копию текста, в котором в которой все слова начинаются с заглавной буквы
+
+Например, hi_first_letters_of_words('мама МЫЛА раму') выдаст 'Мама Мыла Раму'.
+У каждого слова первый символ будет иметь верхний регистр, а остальные символы слова переводятся в нижний регистр."""
+
+    return any_text.title()
